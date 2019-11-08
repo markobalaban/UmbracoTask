7ea2405f1e3dacebe56c2f2f228b85c577a4780f
markobalaban1 <markobalaban1@gmail.com>
2019-11-08 09:54:12 +0100

    Povezano sa controlerom

--- UmbracoSite/App_Data/Models/all.generated.cs
+++ UmbracoSite/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "401efaaf34a630a8")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.4")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
---
+++ UmbracoSite/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,40 @@
+﻿using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+using UmbracoSite.Models;
+using System.Net.Mail;
+
+namespace UmbracoSite.Controllers
+{
+    public class ContactSurfaceController : SurfaceController
+    {
+        public const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/";
+
+        public ActionResult RenderForm()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Contact.cshtml");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitForm(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                SendEmail(model);
+                TempData["ContactSuccess"] = true;
+                return RedirectToCurrentUmbracoPage();
+            }
+
+            return CurrentUmbracoPage();
+        }
+
+        private void SendEmail(ContactModel model)
+        {
+            MailMessage message = new MailMessage(model.EmailAddress, "website@installumbraco.web.local");
+            message.Subject = string.Format("Enquiry from {0} {1} - {2}", model.FirstName, model.LastName, model.EmailAddress);
+            message.Body = model.Message;
+            SmtpClient client = new SmtpClient("127.0.0.1", 25);
+            client.Send(message);
+        }
+    }
+}
